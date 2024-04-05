a3a0372b4240a94168450943c39c7d53f2ec5904
sejoong.park <sejoong.park@navercorp.com>
2024-04-05 22:43:40 +0900

    1

--- src/main/java/assignment/MoinTest/user/entity/UserIdTypeEnum.java
+++ src/main/java/assignment/MoinTest/user/entity/UserIdTypeEnum.java
@@ -1,19 +1,34 @@
 package assignment.MoinTest.user.entity;
 
-import lombok.Getter;
+import java.util.Arrays;
 
-@Getter
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+//TODO: 1. Enum은 딱히 네이밍에 안넣어도 될거 같은데 팀에서 넣으라면 넣으샘 나는 불용어라 생각하는데 enum이라고 한번더 명시하고 싶을수도 있을듯..
+//TODO: 2. ToString은 웬만하면 넣는게 좋음 로깅할때 저놈 없으면 레퍼런스만 찍힘
+@Getter @ToString
+@RequiredArgsConstructor
 public enum UserIdTypeEnum {
 
-    REG_NO(IdType.REG_NO),  //개인 회원
-    BUSINESS_NO(IdType.BUSINESS_NO);    //법인 회원
+    REG_NO("REG_NO"),  //개인 회원
+    BUSINESS_NO("BUSINESS_NO");    //법인 회원
 
+    // TODO: 2 final 붙인건 매우 굳인듯, UserIdTypeEnum.REG_NO.getIdType <-> UserIdType.REG_NO.getId 이런 네이밍 난 선호 갠취
     private final String idType;
 
-    UserIdTypeEnum(String idType){
-        this.idType = idType;
+    public static UserIdTypeEnum findById(String idType) {
+        return Arrays.stream(values())
+            .filter(it -> it.idType.equals(idType))
+            .findAny()
+            .orElse(null);
+        // .orElseThrow(() -> new IllegalArgumentException("idType - %s".formatted(idType))) 예외를 발생시켜도 즣음
+        //.orElse(UserIdTypeEnum.NONE); 이렇겐 어쩔수 없을때 빼곤 잘 안함
     }
 
+
+    // TODO: 1.
     public static class IdType{
         public static final String REG_NO = "REG_NO";
         public static final String BUSINESS_NO = "BUSINESS_NO";
--- src/main/java/assignment/MoinTest/user/service/UserService.java
+++ src/main/java/assignment/MoinTest/user/service/UserService.java
@@ -30,13 +30,22 @@ public class UserService {
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder passwordEncoder;
 
+	// TODO: 1. 재사용 가능할지 구글에 xxx thread safe 라고 쳐보면 애들이 알려줌 가능하다면 재사용 하면 좋음
+    private static final String EMAIL_REGEX = "[_a-z0-9-]+(.[_a-z0-9-]+)*@(?:\\w+\\.)+\\w+$";
+    private static final Pattern EMAIL_REGEX_PATTERN = Pattern.compile(EMAIL_REGEX);
+
+
     @Transactional
     public ResponseEntity<BaseResponse> signup(SignupRequestDto signupRequestDto) {
 
+		// TODO: 2 예외 처리는 https://spring.io/blog/2013/11/01/exception-handling-in-spring-mvc 여기를 참고해보면 좋음
+		// 지금같이 controller -> service -> repository 뎁스 정도면 괜찮은데 svc1 -> svc2 -> svc3 계속 들어가면 svc에서 예외발생했을때 다시 3->2->1로 예외 응답 보내야하는 문제
+		// + BaseResponse라는 뷰모델이 서비스 레이어에 흩어져 의존성 생김 -> 수정어려워짐 -> 대갈 아파짐
+		// 위 링크의 컨트롤러어드바이스 구현해서 서비스레이어에서는 적절한 예외와 예외 코드를 던지면 어드바이스에서 잡아서 적절하게 응답하면 아주 좋음
+		// 시간있으면 구현해보샘
+
         //아이디가 이메일 형식인지 검사
-        String regex = "[_a-z0-9-]+(.[_a-z0-9-]+)*@(?:\\w+\\.)+\\w+$";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(signupRequestDto.getUserId());
+        Matcher matcher = EMAIL_REGEX_PATTERN.matcher(signupRequestDto.getUserId());
 
         if (matcher.matches() == false){
             return BaseResponse.toResponseEntity(ErrorCode.NOT_MATCH_EMAILFORM);
@@ -48,17 +57,25 @@ public class UserService {
             return BaseResponse.toResponseEntity(ErrorCode.DUPLICATE_USER);
         }
 
+
         User user = new User();
         user.setUserId(signupRequestDto.getUserId());
         user.setPassword(passwordEncoder.encode(signupRequestDto.getPassword()));
         user.setName(signupRequestDto.getName());
-        if (signupRequestDto.getIdType().equals("REG_NO")){
-            user.setIdType(UserIdTypeEnum.REG_NO);
-        }else if(signupRequestDto.getIdType().equals("BUSINESS_NO")){
-            user.setIdType(UserIdTypeEnum.BUSINESS_NO);
-        }else {
-            return BaseResponse.toResponseEntity(ErrorCode.WRONG_IDTYPE);
-        }
+
+		// TODO: 3. if else 많이 쓰는거보다 이런식으로 하면 깔끔할듯 그리고 메서드 추출을 좀 하면 좋을듯
+		// 위에는 checkParams(이메일 체크, 아이디 중복 검사), registerUser(user 생성, repo insert)
+		UserIdTypeEnum userIdType = UserIdTypeEnum.findById(signupRequestDto.getIdType());
+		if (userIdType == null) {
+			return BaseResponse.toResponseEntity(ErrorCode.WRONG_IDTYPE);
+		}
+        // if (signupRequestDto.getIdType().equals("REG_NO")){
+        //     user.setIdType(UserIdTypeEnum.REG_NO);
+        // }else if(signupRequestDto.getIdType().equals("BUSINESS_NO")){
+        //     user.setIdType(UserIdTypeEnum.BUSINESS_NO);
+        // }else {
+        //     return BaseResponse.toResponseEntity(ErrorCode.WRONG_IDTYPE);
+        // }
         user.setIdValue(passwordEncoder.encode(signupRequestDto.getIdValue()));
 
         userRepository.save(user);
@@ -70,7 +87,7 @@ public class UserService {
     public ResponseEntity<LoginResponse> login(LoginRequestDto loginRequestDto) {
 
         Optional<User> found = userRepository.findByUserId(loginRequestDto.getUserId());
-        if(!found.isPresent()){
+        if(found.isEmpty()){
             return LoginResponse.toResponseEntity(ErrorCode.USER_NOT_FOUND);
         }
 
